27d5138954e2730db0c4a9ff25437fab7d75667d
SchneiderMan <jschneid94@gmail.com>
2018-10-23 14:24:46 -0700

    Initial file setup, created server.js and mongoose models

---
+++ scripts/seedDB.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/nytreact"
+);
+
+const articleSeed = [
+    {
+        title: "Test Article",
+        author: "Jordan Schneider",
+        synopsis: "Blah blah blah blah blah",
+        date: new Date(Date.now())
+    }
+];
+
+db.Article
+  .remove({})
+  .then(() => db.Article.collection.insertMany(articleSeed))
+  .then(data => {
+      console.log(data.result.n + " records inserted!");
+      process.exit(0);
+  })
+  .catch(err => {
+      console.log(err);
+      process.exit(1);
+  });
