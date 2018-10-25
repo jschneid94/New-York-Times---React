278ca3ad19e06004a3f91d41d5ca4fa5bf35d393
SchneiderMan <jschneid94@gmail.com>
2018-10-24 23:22:03 -0700

    Added grid components

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import { Container, Col, Row } from "../../components/Grid";
+import Card from "../../components/Card";
+import Form from "../../components/Form";
 import "./Home.css";
 
 class Home extends Component {
-    render() {
-        return (
-            <div className="container">
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col size="sm-12">
+            {/* <Card>
+              <Form></Form>
+            </Card> */}
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 };
 
 export default Home;
